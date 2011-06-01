5ab7389ed1b15c4deb5039ee0de2925e4959917c
Robin Spek <r.m.spek@student.utwente.nl>
2011-06-01 12:51:54 +0200

    1.0-14

--- tp/trans/TransInitializer.java
+++ tp/trans/TransInitializer.java
@@ -5,8 +5,10 @@ import tp.util.Log;
 public class TransInitializer {
 	public static boolean sysout = true;
 	public static void main(String[] args){
+		System.out.println("starting");
 		TPSocket sock = Trans.getTrans().createSocket(1, 1, 1);
+		System.out.println("started");
 		String s = new String("martijnishomo");
-		Log.writeLog("  TI", "succes in writing stuff to socket " + sock.writeOut(s.getBytes()), sysout);
+		//Log.writeLog("  TI", "succes in writing stuff to socket " + sock.writeOut(s.getBytes()), sysout);
 	}
 }
