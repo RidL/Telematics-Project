482aa750f13cba7f89cfe25c7f3ca987f10970cb
RidL <robin@robindesk.(none)>
2011-06-21 01:52:29 +0200

    1.1-14

--- tp/link/Tunnel.java
+++ tp/link/Tunnel.java
@@ -42,8 +42,8 @@ public class Tunnel extends Thread implements Link {
 				do{
 	    			sock = serv.accept();
 	    			System.out.println(addr.toString());
-	    			System.out.println(sock.getRemoteSocketAddress().toString().split("/")[0].split(":")[0]);
-	    		}while(!sock.getRemoteSocketAddress().toString().split("/")[0].split(":")[0].equals(addr.toString()));
+	    			System.out.println(sock.getRemoteSocketAddress().toString().split(":")[0].substring(1));
+	    		}while(!sock.getRemoteSocketAddress().toString().split(":")[0].substring(1).equals(addr.toString()));
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
