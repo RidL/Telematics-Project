2f437f22790957d107f867faeae2f0efb62698b5
RidL <robin@robindesk.(none)>
2011-07-01 00:36:39 +0200

    1.3

---
+++ tp/BS/SocketReader.java
@@ -0,0 +1,75 @@
+package tp.BS;
+//package tp.app;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.net.ServerSocket;
+//import java.net.Socket;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//
+///**
+// *
+// * @author jesse
+// */
+//public class SocketReader extends Thread {
+//    
+//    private ServerSocket ssock;
+//    private Socket sock;
+//    private InputStream is;
+//    private FakeSocket fs;
+//
+//    public SocketReader(FakeSocket fs) {
+//        this.fs = fs;
+//        try {
+//            ssock = new ServerSocket(9876);
+//            sock = null;
+//        } catch (IOException ex) {
+//            Logger.getLogger(SocketReader.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//
+//    @Override
+//    public void run() {
+//        while(sock == null) {
+//            try {
+//                sock = ssock.accept();
+//                System.out.println("hoe vaak kom ik hier dan?");
+//                is = sock.getInputStream();
+//            } catch (IOException ex) {
+//                Logger.getLogger(SocketReader.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        }
+//
+//        byte[] bytes = new byte[96];
+//        int dataRead = 0;
+//        try {
+//            dataRead = is.read(bytes);
+//            System.out.println("lees ik data");
+//        }
+//        catch (IOException ex) {
+//            Logger.getLogger(SocketReader.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//        while(dataRead != -1) {
+//            try {
+//                System.out.println("am i writing data into buffer?");
+//                fs.writeIn(bytes);
+//                dataRead = is.read(bytes);
+//            } catch (IOException ex) {
+//                Logger.getLogger(SocketReader.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        }
+//
+//    }
+//
+//    public byte[] read() {
+//        byte[] result = new byte[96];
+//        try {
+//            is.read(result);
+//        } catch (IOException ex) {
+//            Logger.getLogger(SocketReader.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        return result;
+//    }
+//}
---
+++ tp/BS/SocketWriter.java
@@ -0,0 +1,55 @@
+package tp.BS;
+//// TEMPORARY CLASS
+//package tp.app;
+//
+//import java.io.IOException;
+//import java.io.OutputStream;
+//import java.net.Socket;
+//import java.net.UnknownHostException;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//
+///**
+// *
+// * @author jesse
+// */
+//public class SocketWriter extends Thread {
+//    
+//    private Socket sock;
+//    private OutputStream os;
+//    private FakeSocket fs;
+//
+//    public SocketWriter(FakeSocket fs) {
+//        this.fs = fs;
+//        try {
+//            sock = new Socket("127.0.0.1", 9876);
+//            os = sock.getOutputStream();
+//        } catch (UnknownHostException ex) {
+//            Logger.getLogger(SocketWriter.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (IOException ex) {
+//            Logger.getLogger(SocketWriter.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//
+//    public void write(byte[] bytes) {
+//        try {
+//            os.write(bytes);
+//        } catch (IOException ex) {
+//            Logger.getLogger(SocketWriter.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//
+//    @Override
+//    public void run() {
+//        byte[] data = fs.readOut();
+//        while(data != null) {
+//            try {
+//                os.write(data);
+//                data = fs.readOut();
+//            } catch (IOException ex) {
+//                Logger.getLogger(SocketWriter.class.getName()).log(Level.SEVERE, null, ex);
+//            }
+//        }
+//    }
+//
+//}
