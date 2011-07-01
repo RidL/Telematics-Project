0ede61f21ceeea378660422dbaf62763da6c7c96
Spek R.M. (Student) <r.m.spek@student.utwente.nl>
2011-07-01 17:13:13 +0200

    1.2-48

--- tp/trans/Segment.java
+++ tp/trans/Segment.java
@@ -62,7 +62,7 @@ public class Segment {
     
     public Segment(byte[] bytes, boolean doh){
     	this.bytes = bytes;
-    	this.data = new byte[bytes[5]+7];
+    	this.data = new byte[bytes[5]];
     	for(int i=0; i<data.length; i++){
     		this.data[i] = bytes[i+7];
     	}
